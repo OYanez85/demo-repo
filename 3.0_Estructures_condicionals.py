abdf7e9fd35a74b6f979c91387395904aee36f0a
Oscar Yanez-Feijoo <oscar.yanez.feijoo@gmail.com>
2024-02-23 18:45:57 +0100

    changes

--- 3.0_Estructures_condicionals.py
+++ 3.0_Estructures_condicionals.py
@@ -1,4 +1,4 @@
-0. 
+#0. 
 n = 8
 if n > 10:
     print('major que 10')
@@ -7,20 +7,21 @@ elif n < 10:
 else:
     print('igualque10')
 
-1.
+#1.
 n=10
-print(10<n
+print(10<n)
 
-2.
-if n = 10
+#2.El codi de sota donarà error. Per avaluar una condició (igual o no) s’ha de fer ús de l’operador ==
+#if n = 10
 
-3. 
+#3. En aquest cas, estem comprovant si el nom de la posició 1 de la llista noms és‘Laura’.
+# En cas afirmatiu,s’ha d’imprimir ‘nomcorrecte’ i, en cas contrari, s’ha d’imprimir ‘nomincorrecte’.
 noms = ['Ernest', 'Laura', 'Miquel', 'Maria']
 if noms[1]=='Laura':
     print('nom correcte')
-else:print('nomincorrecte')
+else:print('nom incorrecte')
 
-4.
+# 4.En aquest cas, com si existeix la primera posició en la llista “noms” donarà el resultat de “Ernest està present en noms”
 noms = ['Ernest', 'Laura', 'Miquel', 'Maria']
 
 if noms[1] in noms:
@@ -28,21 +29,21 @@ if noms[1] in noms:
 else:
     print(noms[1],'noestàpresentennoms')
 
-5. 
+#5. Aquí estem avaluant si n és més gran o igual que 10, o bé si n és més petit o igual que 5. En el cas que cap de les dues condicions es compleixi, s’imprimirà ‘major que 5 i menor que 10
 if n >= 10:
     print('major o igual que 10')
 elif n<=5:
     print('menor o igual que 5')
 else:
-    print('majorque5imenor que10')
+    print('majorque5imenor que 10')
     
-6. 
+# 6. El que estem fent aquí és, primer, introduir per teclat un nou nom i un número
+# Després,  el  número introduït  el  posem  com  a  condició  i,  segons  si  aquest  número  és  menor  o igual que 20, s’afegirà a la llista el nou nom o, en el cas contrari, s’eliminarà el de l’última posició
 nom_nou = input()
 n=int(input())
 
 noms=['Ernest','Laura','Miquel','Maria']
 
-7.
 if n <= 20:
     noms.append(nom_nou)
 else:
@@ -50,11 +51,17 @@ else:
 
 print(noms)
 
-8.
+#7. Aquí estem avaluant dos valors de dues claus diferents del diccionari. 
+# En el cas que la ciutat sigui Barcelona, s’afegirà una nova clau-valor (‘barri’: ‘Eixample’). 
+# A més, també s’avalua sil’edat és menor que 30 i, en el cas que sigui així, el diccionari tindrà una altra parella clau-valor(‘categoria’:‘Jove’).
+
 dades = {'nom': 'Miquel', 'ciutat': 'Barcelona', 'edat': 28}
+
 if dades['ciutat']=='Barcelona':
     dades['barri']='Eixample'
+
 if dades['edat'] < 30:
     dades['categoria']='Jove'
+
 print(dades)
 
